6755058c8b2e5947f97d115f6049957ea5edf28e
Blue Abad <bibo1011@gmail.com>
2020-09-08 22:45:03 -0700

    fix street view

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,7 +59,7 @@ $(document).ready(function(){
         })
         function clearResults(){
             $("ul").children().remove();
-
+            $("#street").children().remove();
         }
         clearResults()
 
