8b2b9db0191a5310ace957b5785a3a7372b270ea
Blue Abad <bibo1011@gmail.com>
2020-09-08 17:13:56 -0700

    change font setting on result

--- README.md
+++ README.md
@@ -2,9 +2,14 @@
 Site where you look for local breweries 
 
 ## Built with 
-*HTML
-*CSS
-*Javascript
+HTML
+CSS
+Javascript
+
+## Contributors:
+Blue Ramaila abad   https://github.com/bibo1011
+Dharma Teja Koneru  https://github.com/koneruteja
+Daniel Lindsey      https://github.com/DLindsey3415
 
 ## Website:   https://koneruteja.github.io/plenty-of-brew/
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
             document.getElementById("beerBtn").click();
         }
     });
-    $('#beerBtn').bind('click', function() {
+    $('#beerBtn').on('click', function() {
         // alert('User clicked on "foo."');
         var searchCity = document.querySelector("#city").value;
         if (searchCity === '') { 
@@ -26,24 +26,37 @@ $(document).ready(function(){
             var i;
             for (i = 0; i < response.length; i++){
                 console.log(response[i]);
+                var city = JSON.stringify(response[i].city).replace(/"/g, '');
+                var state = JSON.stringify(response[i].state).replace(/"/g, '');
                 var name = JSON.stringify(response[i].name).replace(/"/g, '');
                 var street = JSON.stringify(response[i].street).replace(/"/g, '');
                 var phone = JSON.stringify(response[i].phone).replace(/"/g, '');
                 var type = JSON.stringify(response[i].brewery_type).replace(/"/g, '');
                 var url = JSON.stringify(response[i].website_url).replace(/"/g, '');
-                var list = $("ul").append("<li style='list-style-type:none'><span style='font-weight:bold'>" + name);
-                $("ul").append("<p> Address: " + street + "<br> Phone: " + phone + "<br> Type: " + type + "<br> Link: " + "<a target='_blank' href=>" + url);
-
-                
-                
+                $("h5").text("Breweries around " + city);
+                // var website = document.createElement("a")
+                // website.href = url
+                $("ul").append("<h4 style='list-style-type:none'><span style='font-weight:bold'>" + name);
+                $("ul").append("<p> Address: " + street + "<br> State: " + state + "<br> Phone: " + phone + "<br> Type: " + type);
+                $("ul").append("<p> Link: <a target='_blank' href=>" + url);
+                // $("a").text(website.href)
+                console.log(url);
             }
         })
+        function clearResults(){
+            $("ul").children().remove();
+
+        }
+        clearResults()
 
         
         
     
     });
 });  
+
+
+
 // //set the marquee parameters
 // function init() { 
 //     rtl_marquee.start();
@@ -56,15 +69,3 @@ $(document).ready(function(){
 // rtl_marquee = new xbMarquee('rtl_marquee', '40px', '95%', 6, 80, rtl_marquee_Direction, 'scroll', rtl_marquee_Contents);
 // window.setTimeout( init, 200); 
        
-    
-    
-// search on click
-
-        
-//     })
-// }
-    // fetch(https://api.openbrewerydb.org/breweries?by_city=san_diego).
-// }
-
-// 
-// submitBtn.addEventListener("click", formSubmit);
